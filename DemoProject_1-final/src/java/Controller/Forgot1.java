0065cbd46ed81641bfadcbc25d39a0b0eee12745
NHK1904 <nguyenkhangloveanime@gmail.com>
2023-07-29 14:28:20 +0700

    a
    a

---
+++ DemoProject_1-final/src/java/Controller/Forgot1.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import Model.Account;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ * @author khang
+ */
+public class Forgot1 extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Account a = new Account();
+
+        String repass = req.getParameter("repass");
+        String acnamee = req.getParameter("acname");
+        String passs = req.getParameter("pass");
+
+        if (passs.equals(repass)) {
+            String error = "Change password successfully!";
+            String errorr = "Back Login";
+            req.setAttribute("error", error);
+            req.setAttribute("errorr", errorr);
+            a.changePass(passs, acnamee);
+            req.getRequestDispatcher("Forgot1.jsp").forward(req, resp);
+        } else {
+
+            String error = "Passwords do not match ";
+            req.setAttribute("error", error);
+            req.getRequestDispatcher("Forgot1.jsp").forward(req, resp);
+
+        }
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
